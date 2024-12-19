f5c6e35397e71887cf8b79f4e903145ace98708b
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-12-19 23:58:28 +0530

    switch off turboMode

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // turboMode: true, // Enable Turbopack (experimental bundler for faster builds)
+  experimental: {
+    turboMode: false, // Disable Turbopack and use Webpack instead
+  },
 };
 
 export default nextConfig;
