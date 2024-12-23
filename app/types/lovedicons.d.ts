e31c952d5050e5aa5a5f8dac8c825c808daa7c1a
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-12-23 17:37:31 +0530

    add lovedicons types

--- app/types/lovedicons.d.ts
+++ app/types/lovedicons.d.ts
@@ -46,3 +46,52 @@ declare module "lovedicons/dist/arrO" {
   import React from "react";
   export const ArrChevronDownO: React.FC<React.SVGProps<SVGSVGElement>>;
 }
+
+declare module "lovedicons/dist/secuO" {
+  import React from "react";
+  export const SecuEyeO: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+declare module "lovedicons/dist/educO" {
+  import React from "react";
+  export const EducBrifecaseTickO: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+declare module "lovedicons/dist/desiO" {
+  import React from "react";
+  export const DesiShapesO: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+declare module "lovedicons/dist/userO" {
+  import React from "react";
+  export const UserPeopleO: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+declare module "lovedicons/dist/mediaO" {
+  import React from "react";
+  export const MediaMicrophone2O: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+declare module "lovedicons/dist/mediaO" {
+  import React from "react";
+  export const MediaVideoSquareO: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+declare module "lovedicons/dist/educO" {
+  import React from "react";
+  export const EducTeacherO: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+declare module "lovedicons/dist/educO" {
+  import React from "react";
+  export const EducBookO: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+declare module "lovedicons/dist/educO" {
+  import React from "react";
+  export const EducClipboardO: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+declare module "lovedicons/dist/esseO" {
+  import React from "react";
+  export const EsseMenuO: React.FC<React.SVGProps<SVGSVGElement>>;
+}
