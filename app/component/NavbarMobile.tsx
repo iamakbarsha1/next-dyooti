6e96557dce72060e1213af3482687ac37e57174d
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-12-24 12:43:52 +0530

    add Navbar - Mobile to Tablet

---
+++ app/component/NavbarMobile.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavbarMobile = () => {
+  return (
+    <main className="w-full h-full bg-red-100">
+      <div>NavbarMobile</div>
+    </main>
+  );
+};
+
+export default NavbarMobile;
