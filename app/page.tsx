816000d8c3b154c66e4a9d12acb16544aea76293
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-12-25 00:11:43 +0530

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import heroLandingImage from "/assets/images/landingPageHero.jpg";
+
+import Image from "next/image";
+
 export default function Home() {
-  return <main>Landing page</main>;
+  return (
+    <main className="w-screen bg-red-100">
+      <div className="relative h-[650px]">
+        <div className="absolute z-50 top-0 left-0 bottom-0 right-0 w-[900px] mt-40 text-8xl text-white font-bold text-center">
+          We streamline business processes with ServiceNow
+        </div>
+        <Image
+          // width={1200}
+          // height={400}
+          alt="landingPageHero"
+          src={"/assets/images/landingPageHero.jpg"}
+          priority={true}
+          fill={true}
+        />
+      </div>
+    </main>
+  );
 }
