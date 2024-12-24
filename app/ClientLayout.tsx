0edea069080041add6dd3d67da19f4087473cb5b
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-12-24 12:43:04 +0530

    coment unused useState

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -3,6 +3,7 @@
 
 import React from "react";
 import dynamic from "next/dynamic";
+import NavbarMobile from "./component/NavbarMobile";
 
 const Navbar = dynamic(() => import("./component/Navbar"), {
   ssr: false,
@@ -10,8 +11,12 @@ const Navbar = dynamic(() => import("./component/Navbar"), {
 });
 
 const ClientLayout = ({ children }: { children: React.ReactNode }) => {
+  // const [showSidebar, setShowSidebar] = React.useState<boolean>(false);
   return (
-    <main>
+    <main className="relative">
+      <section className="absolute top-0 left-0 z-50 w-fit h-screen lg:hidden">
+        <NavbarMobile />
+      </section>
       <Navbar />
       {children}
     </main>
